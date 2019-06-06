0c769d895c85bbdb359e3dcd6c3ae4e2d2e21fcc
user <mohsennoghly@gmail.com>
2019-06-06 19:01:39 +0430

    init

---
+++ angularclient/src/app/user-list/user-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user';
+import { UserService } from '../service/user.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+})
+export class UserListComponent implements OnInit {
+
+  users: User[];
+
+  constructor(private userService: UserService) {
+
+  }
+
+  ngOnInit() {
+    this.userService.findAll().subscribe(data => {
+      this.users = data;
+    });
+  }
+}
